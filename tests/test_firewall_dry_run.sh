4092f5c969a6041dc4f789ab42e349fe1584f288
Patrick Schleizer <adrelanos@whonix.org>
2026-04-23 06:37:04 -0400

    fix

--- tests/test_firewall_dry_run.sh
+++ tests/test_firewall_dry_run.sh
@@ -23,7 +23,7 @@ set -o nounset
 set -o errtrace
 set -o pipefail
 
-if ! [ "$CI" = "true" ]; then
+if ! [ "${CI:-}" = "true" ]; then
   printf '%s\n' "$0: These tests are only supposed to run on CI." >&2
   exit 1
 fi
